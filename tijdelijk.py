df81433e045470296601222d7a6a851117c4b995
Sven Wennekers <svenwennekers@gmail.com>
2023-05-05 12:45:05 +0200

    Functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,23 +1,29 @@
-mijn_dictionary = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5,
-}
+from helper import decoreer
 
-aanbieding = mijn_dictionary["aardbei"] * 0.8
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+def print_aanbieding():
+  prijzen = {
+      "aardbei" : 3,
+      "vanille" : 4,
+      "chocolade" : 5,
+  }
 
-reclame_tekst2 = reclame_tekst[:63]
+  Aanbieding = prijzen["aardbei"] * 0.8
+  reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {Aanbieding}"
 
-reclame_tekst3 = reclame_tekst2.upper()
+  reclame_tekst2 = reclame_tekst[:63]
 
-reclame_tekst4 = reclame_tekst3.split(" ")
+  reclame_tekst3 = reclame_tekst2.upper()
 
-for el in reclame_tekst4:
-  if len(el) > 5:
-    print(el.upper())
-  else:
-    print(el.lower())
+  reclame_tekst4 = reclame_tekst3.split(" ")
+
+  for el in reclame_tekst4:
+    if len(el) > 5:
+      print(el.upper())
+    else:
+      print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
     
 
 
